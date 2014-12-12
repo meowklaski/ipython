b5f0d350a40cd60130bb254a1d442922d8294f58
Bussonnier Matthias <bussonniermatthias@gmail.com>
2014-12-12 17:41:39 +0100

    testroup by name

--- IPython/testing/iptestcontroller.py
+++ IPython/testing/iptestcontroller.py
@@ -427,18 +427,12 @@ def prepare_controllers(options):
     """Returns two lists of TestController instances, those to run, and those
     not to run."""
     testgroups = options.testgroups
+    import pdb; pdb.set_trace()
     if testgroups:
         alljs = all_js_groups()
-        l = int(len(alljs)/3)
 
         if 'js' in testgroups:
-            js_testgroups = alljs
-        elif 'js1' in testgroups:
-            js_testgroups = alljs[:l]
-        elif 'js2' in testgroups:
-            js_testgroups = alljs[l:2*l]
-        elif 'js3' in testgroups:
-            js_testgroups = alljs[2*l:]
+            js_testgroups = [g for g in testgroups if g.startswith(testgroups)]
         else:
             js_testgroups = [g for g in testgroups if g.startswith(js_prefix)]
         
